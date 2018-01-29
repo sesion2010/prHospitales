d0dd6313ee853148660d49024cd14cea962dd934
INFTEL 06 <inftel06@imac-de-master-inftel-06.local>
2018-01-29 13:35:20 +0100

    Initial Commit

---
+++ prHospitales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prHospitales
+//
+//  Created by INFTEL 06 on 29/1/18.
+//  Copyright © 2018 INFTEL 06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
